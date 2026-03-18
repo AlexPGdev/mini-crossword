f7d0e660b94a99bb5a7b1dfb4dcaf6d167dec419
AlexPGdev <alexandru82250@gmail.com>
2026-03-18 22:26:08 +0100

    Changed home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className='p-10 justify-center items-center flex flex-col gap-y-10'>
+        <img src='/peepoHey.gif' className='w-32 h-32' />
+        <h1 className='text-4xl'>AlexPG.</h1>
+
+        <h2 className='text-2xl'>Check out my projects!</h2>
+
+        <div className='flex justify-between items-center gap-x-10 text-center'>
+            <div className='flex flex-col gap-y-4 max-w-sm'>
+                <div>
+                    <h3 className='text-xl'>Mini Crossword</h3>
+                    <p className='text-sm'>A mini crossword game</p>
+                </div>
+                
+                <a href='/mini-crossword' className='hover:scale-105 transition-all'>
+                    <img src='/cw.png' className='rounded-xl'></img>
+                </a>
+            </div>
+        </div>
     </div>
   );
 }
