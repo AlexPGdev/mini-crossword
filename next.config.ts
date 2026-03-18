3f62f58e36160b31a0e6aaa7aa899476dbf1b2e3
Alexandru Ungureanu <alexandru82250@gmail.com>
2026-03-18 21:07:34 +0100

    Add assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/mini-crossword',
+  assetPrefix: '/mini-crossword/',
 };
 
 export default nextConfig;
